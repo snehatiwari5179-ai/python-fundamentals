a6892f1546d24d36fb1edc473728bd7f91f5a041
sneha tiwari <sneha.tiwari5179@gmail.com>
2026-07-12 20:38:47 +0530

    Add files via upload
    Operator topic completed.

---
+++ op_precedence.py
@@ -0,0 +1,9 @@
+#standard hierarchy evaluation
+default_calculation=3+4*2
+print("result:",default_calculation)
+#overriding hierarchy using parantheses
+bracket_calculation=(3+4)*2
+print("result:", bracket_calculation)
+#same-rank operators(left to right rule)
+equal_rank_calculation= 10*2/5
+print("result:",equal_rank_calculation)
