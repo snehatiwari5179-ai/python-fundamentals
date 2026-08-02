42760f1d4567b8ac7d7bd73f600dec9c38f7ac0b
sneha tiwari <sneha.tiwari5179@gmail.com>
2026-08-02 19:15:16 +0530

    Add files via upload

---
+++ pass_statement.py
@@ -0,0 +1,20 @@
+# Example 1
+# pass does nothing, it is used as a placeholder for future code, thatswhy no output is generated.
+if True:
+    pass
+
+
+# Example 2
+
+age = 20
+
+if age >= 18:
+    pass
+
+
+# Example 3
+
+age = 15
+
+if age >= 18:
+    pass
