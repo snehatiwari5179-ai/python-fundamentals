083446eca5eff4f6e059121778b43076b921bbf7
sneha tiwari <sneha.tiwari5179@gmail.com>
2026-08-02 20:47:06 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,22 @@
+# while loop
+# example 1
+i=1
+while i<=5:
+    print(i)
+    i += 1  # without this line, the loop will run infinitely because the condition will always be true.
+
+print("___________________________________")
+
+# example 2
+number=10
+while number <=15:
+    print(number)
+    number += 1  # incrementing the number to avoid infinite loop  
+
+print("___________________________________")
+
+# data eng. example
+record=1
+while record <= 5:
+    print(f"Processing record {record}")
+    record += 1  # incrementing the record to avoid infinite loop.
