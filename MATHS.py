e7762f4d03040a3a8825c33fbcee7034c4d40517
sneha tiwari <sneha.tiwari5179@gmail.com>
2026-08-16 20:48:56 +0530

    Add files via upload

---
+++ MATHS.py
@@ -0,0 +1,50 @@
+import math
+# square root
+print(math.sqrt(64))
+
+print("___________________________")
+
+# ceil
+print(math.ceil(8.3)) # Rounds 8.3 upward → 9
+
+print("___________________________")
+
+# floor
+print(math.floor(4.9))   # Rounds 4.9 downward → 4
+
+print("___________________________")
+
+# power
+print(math.pow(3,7))  # Calculates 3⁷ → 2187.0
+
+print("___________________________")
+
+# PI
+print(math.pi)  # Prints the value of π
+
+print("___________________________")
+
+# E
+print(math.e)  # Prints Euler's number e
+
+print("___________________________")
+
+# absolute value
+print(math.fabs(-20))  # Returns absolute value → 20.0
+
+print("___________________________")
+
+# factorial
+print(math.factorial(4))  # calculates 4! → 24
+
+print("___________________________")
+
+# GCD
+print(math.gcd(12,18)) # Finds greatest common divisor
+
+print("___________________________")
+
+# trignometry
+print(math.sin(0))  # Calculates sine of 0 → 0.0
+
+print("___________________________")
